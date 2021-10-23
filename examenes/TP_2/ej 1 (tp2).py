7ecbe008ac7cce2eb4c29b96b91754899b496fe5
NSchteimberg <nschteimberg@gmail.com>
2021-10-23 14:24:00 -0300

    Ej1 Lo reescribí como funciones para facilitar

--- examenes/TP_2/ej 1 (tp2).py
+++ examenes/TP_2/ej 1 (tp2).py
@@ -7,64 +7,69 @@ resultCABA = pd.read_csv (                          # Resultados totales
 )
 resultCABA.head()
 
+#%% [markdown]
+### Funciones para armar tablas de porcentaje por circuito y por comuna
+# La idea es que se puedan aplicar a distintas categorías y agrupaciones
+
+#%%
+def porcentaje_circuito(result_total, result_agrupacion):
+    """
+    Función que a partir de tablas con el caro en disputa y una agrupación en específico, 
+    calcula el porcentaje por circuito electoral
+    """
+
+    # Primero suma la cantidad de votos totales por circuito
+    result_total_acotado = result_total[["CODIGO_CIRCUITO", "CODIGO_MESA", "VOTOS_AGRUPACION", "NOMBRE_REGION", "NOMBRE_AGRUPACION"]]
+
+    result_total_circuito = (result_total_acotado.groupby(["NOMBRE_REGION", "CODIGO_CIRCUITO"]).sum())
+
+    result_total_circuito = result_total_circuito.rename(columns={"VOTOS_AGRUPACION": "VOTOS_TOTALES"})
+
+
+    # Luego suma la cantidad de votos de determinada agrupacion por circuito
+    result_agrupacion_acotado = result_agrupacion[["CODIGO_CIRCUITO", "CODIGO_MESA", "VOTOS_AGRUPACION", "NOMBRE_REGION", "NOMBRE_AGRUPACION"]]
+
+    result_agrupacion_circuito = (result_agrupacion_acotado.groupby(["NOMBRE_REGION", "CODIGO_CIRCUITO"]).sum())
+
+    # Luego hace un merge, y calcula el porcentaje diviendo votos de la agrupacion por totales
+    porcentaje_circuito = pd.merge(result_total_circuito, result_agrupacion_circuito, on=["NOMBRE_REGION","CODIGO_CIRCUITO"])
+
+    porcentaje_circuito["PORCENTAJE_AGRUPACION"] = ((porcentaje_circuito["VOTOS_AGRUPACION"] / porcentaje_circuito["VOTOS_TOTALES"]) * 100).round(2)
+
+
+    return porcentaje_circuito
+#%%
+def porcentaje_comuna(porcentaje_circuito):
+    """
+    Función que en base al porcentaje por circuito
+    calcula el porcentaje por comuna
+    """
+    porcentaje_comuna = (porcentaje_circuito.groupby("NOMBRE_REGION")["VOTOS_TOTALES", "VOTOS_AGRUPACION"].sum())
+    porcentaje_comuna["PORCENTAJE_AGRUPACION"] = ((porcentaje_comuna["VOTOS_AGRUPACION"] / porcentaje_comuna["VOTOS_TOTALES"] * 100).round(2))
+
+    return porcentaje_comuna
+
+#%% [markdown]
+### Porcentajes del FIT para categorías Presidente y Vicepresidente
 #%%
 # Resultados del FIT en cada mesa solo para Presidente
-resultCABA_PRES_FIT = resultCABA[(resultCABA["NOMBRE_CATEGORIA"] == "Presidente y Vicepresidente de la República") & (resultCABA["NOMBRE_AGRUPACION"] == "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD")]
-resultCABA_PRES_FIT.reset_index(inplace=True)
-
-#%% 
-# Me saco de encima las columnas que no me interesan. Igual la verdad es que no sé si es necesario este paso
-resultCABA_PRES_FITa= resultCABA_PRES_FIT[["CODIGO_CIRCUITO", "CODIGO_MESA", "VOTOS_AGRUPACION", "NOMBRE_REGION", "NOMBRE_AGRUPACION"]]
-resultCABA_PRES_FITa
+resultCABA_pres_fit = resultCABA[(resultCABA["NOMBRE_CATEGORIA"] == "Presidente y Vicepresidente de la República") & (resultCABA["NOMBRE_AGRUPACION"] == "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD")]
+resultCABA_pres_fit.reset_index(inplace=True)
 
 #%%
-# Suma los votos del FIT en cada mesa para obtener el total por circuito electoral
-
-votos_fit_por_circuito = (resultCABA_PRES_FITa.groupby(["NOMBRE_REGION", "CODIGO_CIRCUITO"]).sum())
-votos_fit_por_circuito = votos_fit_por_circuito.rename(columns={"VOTOS_AGRUPACION": "VOTOS_FIT"})
-votos_fit_por_circuito
-
-# %%
-# A partir de aquí repito el proceso anterior de agrupar las mesas en circuitos pero con todos los partidos (no solo el FIT), 
-# para poder calcular el porcentaje del FIT en cada circuito
-
-# Me saco de encima las columnas que no me interesa
-resultCABA_circuito = resultCABA[["CODIGO_CIRCUITO", "CODIGO_MESA", "VOTOS_AGRUPACION", "NOMBRE_CATEGORIA", "NOMBRE_AGRUPACION", "NOMBRE_REGION"]]
-resultCABA_circuito_pres = resultCABA_circuito[resultCABA_circuito["NOMBRE_CATEGORIA"] == "Presidente y Vicepresidente de la República"]
-resultCABA_circuito_pres
+# Resultados totales en cada mesa solo para Presidente
+resultCABA_pres_total = resultCABA[resultCABA["NOMBRE_CATEGORIA"] == "Presidente y Vicepresidente de la República"]
+resultCABA_pres_total.reset_index(inplace=True)
 
 #%%
+# Porcentaje del FIT por circuito
+porc_pres_circuito_fit = porcentaje_circuito(resultCABA_pres_total, resultCABA_pres_fit)
+porc_pres_circuito_fit
 
-# Agrupo las mesas en totales por circuito
-votos_circuito_pres = (resultCABA_circuito_pres.groupby(["NOMBRE_REGION", "CODIGO_CIRCUITO"]).sum())
-votos_circuito_pres = votos_circuito_pres.rename(columns={"VOTOS_AGRUPACION": "VOTOS_TOTALES"})
-votos_circuito_pres
-# %%
-# Hago Merge de la tabla con los votos totales y la tabla con los votos del FIT
-
-votos_total_fit = pd.merge(votos_circuito_pres, votos_fit_por_circuito, on=["NOMBRE_REGION","CODIGO_CIRCUITO"])
-votos_total_fit
-# %%
-# Agrego la columna PORCENTAJE_FIT que divide los votos del FIT por sobre el total de votos en cada circuito (y multiplica por 100) obteniendo el porcentaje
-
-votos_total_fit["PORCENTAJE_FIT"] = ((votos_total_fit["VOTOS_FIT"] / votos_total_fit["VOTOS_TOTALES"]) * 100).round(2)
-
-votos_total_fit # Esta es la tabla que tiene el porcentaje de FIT en cada circuito
-votos_total_fit.sort_values(by="PORCENTAJE_FIT")
-
-
-
-# %%
-# Agrupo los circuitos en cada Comuna, y luego calculo el porcentaje
- 
-votos_comuna = (votos_total_fit.groupby("NOMBRE_REGION")["VOTOS_TOTALES", "VOTOS_FIT"].sum())
-votos_comuna["PORCENTAJE_FIT"] = ((votos_comuna["VOTOS_FIT"] / votos_comuna["VOTOS_TOTALES"] * 100).round(2))
-votos_comuna # Esta es la tabla que tiene el porcentaje de FIT en cada comuna
-# %%
-# Las dos tablas que hice hasta ahora, ordenadas de mayor a menor. 
-# Podría verse como georeferenciarlas
-votos_total_fit.sort_values(by="PORCENTAJE_FIT", ascending=False)
-votos_comuna.sort_values(by=["PORCENTAJE_FIT"], ascending=False)
+#%%
+# Porcentaje del FIT por comuna
+porc_pres_comuna_fit = porcentaje_comuna(porc_pres_circuito_fit)
+porc_pres_comuna_fit
 
 #%%
 # Pendiente: 

41ce226c73573afc696b9e91bba83458e2cd42b7
Florencia Simeran <florenciasimeran@gmail.com>
2021-11-01 12:37:50 -0300

    cambiando y agregando graficos

--- examenes/TP 2/Graficos/Graficos_FIT.py
+++ examenes/TP 2/Graficos/Graficos_FIT.py
@@ -14,8 +14,20 @@ sns.relplot(x = 'VOTOS_FIT_DIPNAC', y = 'NOMBRE_REGION', data= votos_dipnac_graf
 #Grafico votos presidenciales por comuna partido FIT
 votos_pres_grafico_1 = pd.read_csv("pres_FIT_comuna.csv")
 sns.set(style = 'darkgrid')
-sns.relplot(x = 'VOTOS_AGRUPACION', y = 'NOMBRE_REGION', data= votos_pres_grafico_1, row="Votos Agrupación", col= "Comunas", )
+sns.relplot(x = 'VOTOS_AGRUPACION', y = 'NOMBRE_REGION', data= votos_pres_grafico_1)
 
+# %%
+#Gráfico de barras. Porcentaje de votos diputados por partido en cada comuna.
+colors = ["#C0C0C0", "#000000", "#FF0000", "#800000", "#FFFF00", "#808000", "#00FF00", "#008000", "#00FFFF", "#008080","#0000FF", "#000080", "#FF00FF", "#800080", "#FFFFFF"]
+df =  pd.read_csv('DipNac_FIT_comuna.csv')
+eje_x = df["NOMBRE_REGION"]
+eje_y = df["PORCENTAJE_FIT_DIPNAC"]
+plt.bar(eje_x, eje_y, color=colors)
+plt.xticks(rotation=90)
+plt.ylabel('Porcentajes a voto diputados')
+plt.xlabel('Comunas')
+plt.title('Porcentaje de votos al FIT por comuna')
+plt.show()
 # %%
 #Grafico votos presidenciales por comuna partido FIT
 votos_pres_grafico_1 = pd.read_csv("pres_FIT_comuna.csv",)
@@ -149,80 +161,3 @@ plt.ylabel('Porcentajes a voto presidencial')
 plt.xlabel('Comunas')
 plt.title('Porcentaje de votos a ULD por comuna')
 plt.show()
-
-# %%
-colors = ["#C0C0C0", "#000000", "#FF0000", "#800000", "#FFFF00", "#808000", "#00FF00", "#008000", "#00FFFF", "#008080","#0000FF", "#000080", "#FF00FF", "#800080", "#FFFFFF"]
-df =  pd.read_csv('Data_Frame_General_Sin_Comuneros.csv')
-eje_x = df["Partido"]
-eje_y = df["Porc_Pres"]
-plt.bar(eje_x, eje_y, color=colors)
-plt.xticks(rotation=90)
-plt.ylabel('Porcentajes a voto presidencial')
-plt.xlabel('Comunas')
-plt.title('Porcentaje de votos blancos por comuna')
-plt.show()
-# %%
-labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-men_means = [20, 34, 30, 35, 27]
-women_means = [25, 32, 34, 20, 25]
-
-x = np.arange(len(labels))  # the label locations
-width = 0.35  # the width of the bars
-
-fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, men_means, width, label='Men')
-rects2 = ax.bar(x + width/2, women_means, width, label='Women')
-
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
-
-ax.bar_label(rects1, padding=3)
-ax.bar_label(rects2, padding=3)
-
-fig.tight_layout()
-
-plt.show()
-# %%
-
-
-
-#Grafico comparacion de votos a diputados a FIT y FDT por comuna
-
-# Fixing random state for reproducibility
-
-df =  pd.read_csv('Data_Frame_General_Sin_Comuneros.csv')
-
-plt.rcdefaults()
-fig, ax = plt.subplots()
-
-# Example data
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(people))
-performance = 3 + 10 * np.random.rand(len(people))
-error = np.random.rand(len(people))
-
-ax.barh(y_pos, performance, xerr=error, align='center')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(people)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Performance')
-ax.set_title('How fast do you want to go today?')
-
-plt.show()
-
-# %%
-colors = ["#C0C0C0", "#000000", "#FF0000", "#800000", "#FFFF00", "#808000", "#00FF00", "#008000", "#00FFFF", "#008080","#0000FF", "#000080", "#FF00FF", "#800080", "#FFFFFF"]
-df =  pd.read_csv('distrib_DipNac_comuna')
-eje_x = df["NOMBRE_REGION"]
-eje_y = df["VOTOS_FDT_DIPNAC"]
-plt.bar(eje_x, eje_y, color=colors)
-plt.xticks(rotation=90)
-plt.ylabel('Porcentajes a voto presidencial')
-plt.xlabel('Comunas')
-plt.title('Porcentaje de votos blancos por comuna')
-plt.show()
-# %%

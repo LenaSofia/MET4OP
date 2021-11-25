99a3b724fab1242f4d9b61640f2dc6ed12b244bf
Lena Sofía <lenasofia51@gmail.com>
2021-11-25 00:18:25 -0300

    Elimino pasar nombres como lista para nombrar variables

---
+++ examenes/TP3/bootstrap_funcion_combinada.py
@@ -0,0 +1,144 @@
+# %%
+from os import replace
+import numpy as np
+import pandas as pd
+from numpy import fabs, random
+from patsy import dmatrix, dmatrices
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#%%
+
+# Definimos las funciones:
+
+# %%
+
+def armar_DF(x, y):
+    data_frame = pd.DataFrame(list(zip(x, y)), columns=['x', 'y'])
+    return data_frame
+
+#%%
+
+def generar_bootstrap(DF, repeticiones):
+
+    DF_coeficientes = pd.DataFrame()
+
+    for repeticion in range(0, repeticiones):
+        # Construye una nueva lista "DF_sampleado" a partir del sampleo con repeticiones de DF
+        DF_sampleado = DF.sample(n=len(DF), replace=True, random_state=None, axis=0,
+                                 ignore_index=False)
+
+        # Agrega la constante para calcular regresión
+        DF_sampleado = sm.add_constant(DF_sampleado)
+
+        reg = sm.OLS(DF_sampleado["y"], DF_sampleado[["x", "const"]]).fit()
+
+        coef = reg.params
+        coef_DF = coef.to_frame()
+        coef_DF = coef_DF.transpose()
+
+        # Agrego el DF del coeficiente actual al DF final
+        DF_coeficientes = DF_coeficientes.append(coef_DF, ignore_index=True)
+
+    DF_coeficientes = DF_coeficientes.rename(columns={'x': 'coef_x',
+                                                    'const': 'coef_const'})
+
+    return DF_coeficientes
+
+#%%
+
+def sacar_cuantiles(*args):
+
+    cuantiles = pd.DataFrame()
+
+    for argumento in args:
+        cuantiles[argumento.columns] = argumento.quantile([0.025, 0.25, 0.5, 0.75, 0.975])
+
+    return cuantiles
+
+#%%
+
+def hacer_graficos(*args):
+
+    if len(args) == 1:
+        sns.histplot(args)
+
+    else:
+        indice = 0
+        fig, axes = plt.subplots(1, 2, sharey=True, squeeze=True, figsize=(12, 7))
+        for argumento in args:
+            sns.histplot(argumento, ax=axes[indice])
+            indice += 1
+
+    plt.show()
+
+#%%
+
+# Predicción del y a partir de un nuevo x:
+
+def predecir_y_nuevo(x_nuevo, DF_coeficientes):
+
+    DF_y_predichos = DF_coeficientes.copy(deep=True)
+
+    DF_y_predichos = DF_y_predichos.assign(x_nuevo=x_nuevo, y_predicho="")
+
+
+    for fila in range(0, len(DF_y_predichos)):
+        DF_y_predichos.loc[DF_y_predichos.index.get_loc(fila), "y_predicho"] = \
+            DF_y_predichos.loc[DF_y_predichos.index.get_loc(fila), "coef_const"] \
+            + (DF_y_predichos.loc[DF_y_predichos.index.get_loc(fila), "coef_x"]
+               * DF_y_predichos.loc[DF_y_predichos.index.get_loc(fila), "x_nuevo"])
+
+    DF_y_predichos["y_predicho"] = DF_y_predichos.y_predicho.astype(float)
+
+    return DF_y_predichos
+
+
+#%%
+
+def encontrar_coeficientes(x, y, repeticiones):
+
+    DataFrame = armar_DF(x, y)
+
+    DF_bootstrap_coef = generar_bootstrap(DataFrame, repeticiones)
+
+    cuantiles_DF_bootstrap = sacar_cuantiles(DF_bootstrap_coef[['coef_const']], DF_bootstrap_coef[['coef_x']])
+
+    DF_bootstrap_graf = hacer_graficos(DF_bootstrap_coef[['coef_const']], DF_bootstrap_coef[['coef_x']])
+
+    return DF_bootstrap_coef
+
+#%%
+
+
+#Prueba :
+
+auto = pd.read_csv('Anteriores/auto.csv')
+x = auto['weight'].tolist()
+y = auto['price'].tolist()
+
+#%%
+
+DF_autos = armar_DF(x, y)
+
+DF_coeficiente_autos = generar_bootstrap(DF_autos, 1000)
+
+#%%
+
+cuantiles_auto = sacar_cuantiles(DF_coeficiente_autos[['coef_const']], DF_coeficiente_autos[['coef_x']])
+
+
+#%%
+
+graf_autos = hacer_graficos(DF_coeficiente_autos['coef_x'], DF_coeficiente_autos['coef_const'])
+
+#%%
+
+DF_y_predichos = predecir_y_nuevo(2450, DF_coeficiente_autos)
+
+cuantiles_y_predicho = sacar_cuantiles(DF_y_predichos[["y_predicho"]])
+
+graf_y = hacer_graficos(DF_y_predichos['y_predicho'])
